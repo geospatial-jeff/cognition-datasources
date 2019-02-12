bc625d3eb4d3d3a73d1c7a813ac17669224c1e9f
Jeff Albrecht <jeff.albrecht@slingshotaerospace.com>
2019-02-12 15:27:43 -0700

    Adding static files to setuptools

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ setup(name="cognition_datasources",
       version='0.1',
       author='Jeff Albrecht',
       author_email='geospatialjeff@gmail.com',
-      packages=find_packages(exclude=['tests', 'static/cbers/cbers_reference.geojson', 'static/naip/coverages', 'utils', 'docs']),
+      packages=find_packages(exclude=['tests', 'datasources/static/cbers/cbers_reference.geojson', 'datasources/static/naip/coverages', 'docs']),
       install_requires = requirements,
       entry_points= {
           "console_scripts": [
-              "cognition-datasources=scripts._cli:cognition_datasources"
-          ]
-      }
+              "cognition-datasources=datasources.scripts._cli:cognition_datasources"
+          ]},
+      include_package_data=True
       )

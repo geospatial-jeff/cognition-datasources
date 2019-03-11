355f8a401358822cab26d82418fa3f5ea4a83e5d
Jeff Albrecht <jeff.albrecht@slingshotaerospace.com>
2019-03-10 21:05:27 -0600

    referencing in init

--- datasources/sources/__init__.py
+++ datasources/sources/__init__.py
@@ -5,7 +5,8 @@ from .sentinel2 import Sentinel2
 from .srtm import SRTM
 from .cbers import CBERS
 from .elevationtiles import ElevationTiles
+from .usgs3dep import USGS3DEP
 
 from .microsoft_building_footprints import MicrosoftBuildingFootprints
 
-all = [Landsat8, NAIP, Sentinel1, Sentinel2, SRTM, CBERS, ElevationTiles, MicrosoftBuildingFootprints]
+all = [Landsat8, NAIP, Sentinel1, Sentinel2, SRTM, CBERS, USGS3DEP, ElevationTiles, MicrosoftBuildingFootprints]

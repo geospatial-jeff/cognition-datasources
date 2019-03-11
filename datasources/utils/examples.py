41797b80589978113fad8119c3c278f1cf782b16
Jeff Albrecht <jeff.albrecht@slingshotaerospace.com>
2019-03-11 01:14:17 -0600

    reworking build, adding function to validate examples with STACValidator

--- datasources/utils/examples.py
+++ datasources/utils/examples.py
@@ -1,21 +1,32 @@
 import json
+import os
+
+from stac_validator.stac_validator import StacValidate
 
 from datasources.sources import all, collections
 from datasources import Manifest
 
 # STAC doesn't work with vector (yet)
 sources = list(set(all) - set(collections.vector))
+example_dir = '../../docs/examples/raster'
 
 def build_examples():
     manifest = Manifest()
     manifest.load_sources(*sources)
-    responses = [x.example() for x in manifest.sources]
-    for response in responses:
-        datasource = list(response.keys())[0]
-        feature_collection = response[datasource]
-        first_feature = feature_collection['features'][0]
-        with open('../../docs/examples/{}.json'.format(datasource), 'w+') as outfile:
+    for source in manifest.sources:
+        example = source.example()
+        first_feature = example[source.__class__.__name__]['features'][0]
+        with open(os.path.join(example_dir, '{}.json'.format(source.__class__.__name__)), 'w+') as outfile:
             json.dump(first_feature, outfile, indent=2)
 
+def validate_examples():
+    valids = []
+    example_files = [os.path.join(example_dir, x) for x in os.listdir(example_dir)]
+    for item in example_files:
+        response = json.loads(StacValidate(item).run()[1:-1])
+        if response['valid_stac']:
+            valids.append(os.path.splitext(os.path.split(item)[-1])[0])
+        else:
+            print("INVALID!!", response)
 
-build_examples()
+    print("Valid datasources: {}".format(valids))

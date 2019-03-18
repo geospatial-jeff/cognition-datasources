babc1216e3f34303a080a5272e9477d0ef46c289
Jeff Albrecht <jeff.albrecht@slingshotaerospace.com>
2019-03-18 12:59:37 -0600

    updating templates

--- datasources/template/template.py
+++ datasources/template/template.py
@@ -5,7 +5,7 @@ from datasources.sources.base import Datasource
 
 class __TEMPLATENAME__(Datasource):
 
-    stac_compliant = True
+    stac_compliant = False
     tags = ['tag1', 'tag2']
 
     def __init__(self, manifest):
@@ -13,8 +13,17 @@ class __TEMPLATENAME__(Datasource):
 
     def search(self, spatial, temporal=None, properties=None, limit=10, **kwargs):
         stac_query = STACQuery(spatial, temporal)
-        pass
 
+        api_request = #Parse stac_query into API-compatible JSON request, append to manifest
 
-    def execute(self, query):
-        pass
+        self.manifest.searches.append([self, api_request])
+
+    def execute(self, api_request):
+        response = # api response
+
+        if self.stac_compliant:
+            # Return output as is (should be Feature Collection)
+            return response
+        else:
+            # Parse api response into STAC-compliant item and return as list of STAC Items
+            return [stac_item]
--- datasources/template/tests.py
+++ datasources/template/tests.py
@@ -1,5 +1,11 @@
-from datasources.tests import BaseTestCases
+from datasources import tests
 
+from __TEMPLATENAME__ import __TEMPLATENAME__
 
-class __TEMPLATENAME__TestCases(BaseTestCases):
+class __TEMPLATENAME__TestCases(tests.BaseTestCases):
 
+    def _setUp(self):
+
+        self.datasource = __TEMPLATENAME__
+        self.spatial = # geojson geometry here
+        self.temporal = # (start_date, end_date) here

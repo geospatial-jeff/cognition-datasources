c2cba8f095f0f8163959977e1eab1c1231af2e0d
Jeff Albrecht <jeff.albrecht@slingshotaerospace.com>
2019-03-17 22:08:29 -0600

    automatically load all datasources in sources folder on manifest creation

--- datasources/manifest.py
+++ datasources/manifest.py
@@ -1,26 +1,29 @@
 from multiprocessing import Process, Pipe
 
-from datasources import sources
+from datasources.sources import collections
 
 class Manifest(dict):
 
-    def __init__(self):
+    # shouldn't use mutable default
+    def __init__(self, tags=['all']):
         super().__init__()
         self.searches = []
+        self.load_sources(tags)
 
     @property
     def sources(self):
         for (k,v) in self.items():
             yield v
 
-    def load_source(self, source):
-        if type(source) == str:
-            self.update({source: getattr(sources, source)(self)})
-        else:
+    def load_sources(self, tags):
+        tagged = []
+        for tag in tags:
+            tagged += getattr(collections, tag)
+        tagged = list(set(tagged))
+
+        for source in tagged:
             self.update({source.__name__: source(self)})
 
-    def load_sources(self, *args):
-        list(map(lambda n: self.load_source(n), args))
 
     def flush(self):
         self.searches = []
@@ -58,7 +61,7 @@ class Manifest(dict):
         for parent_connection in parent_connections:
             resp = parent_connection.recv()
             if resp['stac_items']:
-                stac_compliant = getattr(sources, resp['source']).stac_compliant
+                stac_compliant = self[resp['source']].stac_compliant
                 if stac_compliant:
                     response.update({resp['source']: resp['stac_items']})
                 else:
--- datasources/sources/__init__.py
+++ datasources/sources/__init__.py
@@ -1,21 +1,22 @@
-from .landsat8 import Landsat8
-from .naip import NAIP
-from .sentinel1 import Sentinel1
-from .sentinel2 import Sentinel2
-from .srtm import SRTM
-from .cbers import CBERS
-from .elevationtiles import ElevationTiles
-from .usgs3dep import USGS3DEP
-from .dg_open_data import DGOpenData
 
-from .microsoft_building_footprints import MicrosoftBuildingFootprints
-
-all = [DGOpenData, Landsat8, NAIP, Sentinel1, Sentinel2, SRTM, CBERS, USGS3DEP, ElevationTiles, MicrosoftBuildingFootprints]
 
 class collections(object):
 
+    def load_sources():
+        import os
+        source_files = os.listdir(os.path.dirname(__file__))
+        sources = []
+        for item in source_files:
+            if item.endswith('.py') and item != '__init__.py' and item != 'base.py':
+                module = os.path.splitext(item)[0]
+                imported = getattr(__import__("datasources.sources.{}".format(module), fromlist=[module]), module)
+                sources.append(imported)
+        return sources
+
+    all = load_sources()
     eo = [x for x in all if 'EO' in x.tags]
     ms = [x for x in all if 'MS' in x.tags]
+    sar = [x for x in all if 'SAR' in x.tags]
     satellite = [x for x in all if 'Satellite' in x.tags]
     aerial = [x for x in all if 'Aerial' in x.tags]
     elevation = [x for x in all if 'Elevation' in x.tags]

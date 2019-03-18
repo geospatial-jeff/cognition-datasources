163431b871a577546eca04cbd62bf835e5e0f6b2
Jeff Albrecht <jeff.albrecht@slingshotaerospace.com>
2019-03-18 13:18:43 -0600

    updating requirements and setup

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name="cognition_datasources",
       version='0.1',
       author='Jeff Albrecht',
       author_email='geospatialjeff@gmail.com',
-      packages=find_packages(exclude=['tests', 'datasources/static/cbers/cbers_reference.geojson', 'datasources/static/naip/coverages', 'docs']),
+      packages=find_packages(exclude=['docs']),
       install_requires = requirements,
       entry_points= {
           "console_scripts": [

b6211ee8b69889dc516c6d965436e594816f8847
Jeff Albrecht <jeff.albrecht@slingshotaerospace.com>
2019-04-05 23:38:19 -0600

    updating name of handler

--- driver/handler.py
+++ driver/handler.py
@@ -1,6 +1,6 @@
 from datasources import Manifest
 
-def handler(event, context):
+def __TEMPLATENAME__(event, context):
     manifest = Manifest()
     manifest['__TEMPLATENAME__'].search(event['spatial'], event['temporal'], event['properties'], **event['kwargs'])
     response = manifest.execute()

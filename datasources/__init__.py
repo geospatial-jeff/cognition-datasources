3b4b3f53a5ee123d7f977cc55f31cb1e48ff8cd1
Jeff Albrecht <geospatialjeff@gmail.com>
2019-04-28 18:26:14 -0700

    updating layer arn

--- datasources/__init__.py
+++ datasources/__init__.py
@@ -2,4 +2,4 @@ from . import sources
 from .manifest import Manifest
 
 
-layer_arn = 'arn:aws:lambda:us-east-1:725820063953:layer:cognition-datasources:5'
+layer_arn = 'arn:aws:lambda:us-east-1:725820063953:layer:cognition-datasources:6'

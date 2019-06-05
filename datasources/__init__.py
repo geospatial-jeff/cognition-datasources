833e4f63192d2ab055153960e84e4edc18a97353
Loaner Laptop <geospatialjeff@gmail.com>
2019-06-05 12:15:57 -0700

    bumping cognition layer arn

--- datasources/__init__.py
+++ datasources/__init__.py
@@ -2,4 +2,4 @@ from . import sources
 from .manifest import Manifest
 
 
-layer_arn = 'arn:aws:lambda:us-east-1:725820063953:layer:cognition-datasources:6'
+layer_arn = 'arn:aws:lambda:us-east-1:725820063953:layer:cognition-datasources:7'
